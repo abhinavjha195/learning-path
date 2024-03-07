7b1d8590853442a1dc03166af134be4de20e4c08
abhinavjha195 <abhinav.jha195@gmail.com>
2024-03-07 19:10:56 +0530

    changes-07-03-24

---
+++ lps-new/js/menu.js
@@ -0,0 +1,35 @@
+jQuery(function ($) {
+
+    $(".sidebar-dropdown > a").click(function() {
+  $(".sub-menu").slideUp(200);
+  if (
+    $(this)
+      .parent()
+      .hasClass("active")
+  ) {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .parent()
+      .removeClass("active");
+  } else {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .next(".sub-menu")
+      .slideDown(200);
+    $(this)
+      .parent()
+      .addClass("active");
+  }
+});
+
+$("#close-sidebar").click(function() {
+  $(".page-wrapper").removeClass("toggled");
+});
+$("#show-sidebar").click(function() {
+  $(".page-wrapper").addClass("toggled");
+});
+
+
+   
+   
+});
